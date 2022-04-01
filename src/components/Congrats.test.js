ba7279853e0bbd52dbb3bfb0c313fa009e72f54a
Gabriel Oliveira <gabriel.oliveira2@ab-inbev.com>
2022-04-01 16:31:00 -0300

    configuring some setups

--- src/components/Congrats.test.js
+++ src/components/Congrats.test.js
@@ -1,11 +1,7 @@
 /* eslint-disable testing-library/await-async-query */
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
-
+import { shallow } from 'enzyme';
 import { findByTestAttr } from '../../test/testUtils';
 import Congrats from './Congrats';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const setup = (props = {}) => {
     return shallow(<Congrats {...props} />)
